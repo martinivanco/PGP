8d73c0c56bf2058c27e93bb79ef6d17141c32390
Martin Ivanco <ivancom.fr@gmail.com>
2018-12-19 12:24:20 +0100

    Simple ball with lights

--- src/fractal.vs
+++ src/fractal.vs
@@ -1,7 +1,12 @@
 #version 410 core
 layout(location = 0) in vec3 vertexPosition;
+layout(location = 1) in vec2 uvPosition;
+out vec2 pixelPosition;
+uniform vec2 resolution;
+uniform float time;
 
 void main(){
     gl_Position.xyz = vertexPosition;
     gl_Position.w = 1.0;
+    pixelPosition = uvPosition;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
+#include <chrono>
 #include <stdio.h>
 #include <stdlib.h>
+#include <chrono>
 
 #include <GL/glew.h>
-
 #include <GLFW/glfw3.h>
 GLFWwindow* window;
-
 #include <glm/glm.hpp>
 using namespace glm;
 
@@ -52,35 +52,62 @@ int main( void ) {
 
 	GLuint programID = LoadShaders("src/fractal.vs", "src/fractal.fs");
 
-	static const GLfloat g_vertex_buffer_data[] = { 
+	static const GLfloat vertices[] = {
+		-1.0f,  1.0f, 0.0f,
 		-1.0f, -1.0f, 0.0f,
-		 1.0f, -1.0f, 0.0f,
 		 1.0f,  1.0f, 0.0f,
-		-1.0f,  1.0f, 0.0f
+		 1.0f, -1.0f, 0.0f
+	};
+
+	static const GLshort uvs[] = {
+		  0,   0,
+		  0, 600,
+		800,   0,
+		800, 600
 	};
 
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	static const unsigned short indices[] = {0, 1, 3, 2, 1, 3};
-	GLuint elementbuffer;
-	glGenBuffers(1, &elementbuffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+	GLuint uvbuffer;
+	glGenBuffers(1, &uvbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(uvs), uvs, GL_STATIC_DRAW);
+
+	static const unsigned short indices[] = {0, 1, 2, 3};
+	GLuint indexbuffer;
+	glGenBuffers(1, &indexbuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbuffer);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+	glUseProgram(programID);
+	GLuint resID = glGetUniformLocation(programID, "resolution");
+	GLuint timeID = glGetUniformLocation(programID, "time");
+
+	std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 	do {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(programID);
-
+		glUniform2f(resID, 800, 600);
+		std::chrono::duration<double> diff = std::chrono::system_clock::now() - start;
+		glUniform1f(timeID, diff.count() * 0.1);
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned short), GL_UNSIGNED_SHORT, (void*)0);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+		glVertexAttribPointer(1, 2, GL_SHORT, GL_FALSE, 0, (void*)0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbuffer);
+
+		glDrawElements(GL_TRIANGLE_STRIP, sizeof(indices) / sizeof(unsigned short), GL_UNSIGNED_SHORT, (void*)0);
+
 		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
